79c1362816d203524e129a4b76de5ccdd57a37ff
Chris Gilmer <chris@truss.works>
2020-01-14 11:25:02 -0800

    Update terraform 0.12 syntax

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@
 #
 
 data "aws_sns_topic" "main" {
-  name = "${var.sns_topic_name}"
+  name = var.sns_topic_name
 }
 
 #
@@ -33,21 +33,21 @@ data "aws_sns_topic" "main" {
 resource "aws_cloudwatch_event_rule" "main" {
   name          = "cloudtrail-api-events"
   description   = "AWS CloudTrail API events"
-  event_pattern = "${file("${path.module}/event-pattern.json")}"
+  event_pattern = file("${path.module}/event-pattern.json")
 }
 
 resource "aws_cloudwatch_event_target" "main" {
-  rule      = "${aws_cloudwatch_event_rule.main.name}"
+  rule      = aws_cloudwatch_event_rule.main.name
   target_id = "send-to-sns"
-  arn       = "${data.aws_sns_topic.main.arn}"
+  arn       = data.aws_sns_topic.main.arn
 
   input_transformer {
     input_paths = {
-      event = "$.detail.eventName"
-
+      event      = "$.detail.eventName"
       parameters = "$.detail.requestParameters"
     }
 
     input_template = "\"AWS CloudTrail Modification: <event> with request parameters: <parameters>\""
   }
 }
+
